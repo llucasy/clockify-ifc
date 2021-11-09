4176008b195c501a81fe360cc1ded7a5fe9231be
llucasy <llucasy@hotmail.com>
2021-11-09 17:03:00 -0300

    changes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (!key) {
     d = new Date();
 
     if (d.getDay() != 0 && d.getDay() != 6) {
-      if (d.getHours() == 16 && d.getMinutes() == 5) {
+      if (d.getHours() == 17 && d.getMinutes() == 6) {
         async function process(key, i, h) {
           console.log("entrou");
 
